2a035a84b0def316c140dc91eb9202fb4d5f86b1
Venu Sai <venusaim23@gmail.com>
2022-01-01 21:13:56 +0530

    initial changes

---
+++ app/src/main/java/com/mlh/goalsops/AddResolution.java
@@ -0,0 +1,33 @@
+package com.mlh.goalsops;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.mlh.goalsops.databinding.FragmentAddResolutionBinding;
+
+public class AddResolution extends BottomSheetDialogFragment {
+
+    private FragmentAddResolutionBinding binding;
+
+    public AddResolution() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAddResolutionBinding.inflate(inflater, container, false);
+
+
+        return binding.getRoot();
+    }
+}
